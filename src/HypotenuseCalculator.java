a641e60feb668d8c9c1a7e227ae1e75555742920
MehmetEmin94 <emin.aykan.ea@gmail.com>
2021-06-18 16:02:00 +0300

    18.06.2021

---
+++ src/HypotenuseCalculator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class HypotenuseCalculator {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+
+        System.out.println("Please enter first steep edge : ");
+        int firstEdge=scan.nextInt();
+        System.out.println("Please enter first steep edge : ");
+        int secondEdge= scan.nextInt();
+
+        double hypotenuse=Math.sqrt((firstEdge*firstEdge)+(secondEdge*secondEdge));
+        System.out.println(hypotenuse);
+    }
+}
