a8c9b5920bd94d074bacd405305a1a89c5849f96
MehmetEmin94 <emin.aykan.ea@gmail.com>
2021-06-22 18:08:50 +0300

    18.06.2021

---
+++ src/FindPrimeNumbers.java
@@ -0,0 +1,16 @@
+public class FindPrimeNumbers {
+    public static void main(String[] args) {
+
+        for(int i=2;i<100;i++){
+            int num=0;
+           for(int j=1;j<=i;j++){
+               if (i%j==0){
+                   num++;
+               }
+            }
+           if(num==2){
+               System.out.println("Prime number"+i);
+           }
+        }
+    }
+}
