61bb90fa965aa3cd4859f8a88db604915493d95c
MehmetEmin94 <emin.aykan.ea@gmail.com>
2021-06-22 20:29:34 +0300

    18.06.2021

---
+++ src/Row.java
@@ -0,0 +1,9 @@
+public class Row {
+    public static void main(String[] args) {
+        int x=2;
+        for (int i=0;i<5;i++){
+            x*=2;
+            System.out.println(x);
+        }
+    }
+}
