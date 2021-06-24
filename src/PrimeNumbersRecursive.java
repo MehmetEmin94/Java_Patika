892c0fc9d734cedf3b2b8eff48408c2861973c24
MehmetEmin94 <emin.aykan.ea@gmail.com>
2021-06-24 12:57:39 +0300

    18.06.2021

---
+++ src/PrimeNumbersRecursive.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PrimeNumbersRecursive {
+    static boolean isPrime(int number,int i){
+        if(number<=2)
+            return (number==2)?true:false;
+
+        if(number%i==0)
+            return false;
+        if (i*i>number)
+            return true;
+        return isPrime(number,i+1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter number : ");
+        int number= scan.nextInt();
+
+        if(isPrime(number,2)){
+            System.out.println("Yes");
+        }else {
+            System.out.println("No");
+        }
+
+
+    }
+}
