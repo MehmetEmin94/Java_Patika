149dc6d0980b11953485f99552d859bcc29abb25
MehmetEmin94 <emin.aykan.ea@gmail.com>
2021-06-25 23:45:24 +0300

    18.06.2021

---
+++ src/java101_arrays/practice/FindRepetitiveNums.java
@@ -0,0 +1,36 @@
+package java101_arrays.practice;
+
+import java.util.Arrays;
+
+public class FindRepetitiveNums {
+
+    static boolean isFind(int[] arr,int value){
+        for(int i:arr){
+            if (i==value){
+                return true;
+            }
+        }return false;
+    }
+    public static void main(String[] args) {
+
+        int[] arr = {3, 7, 3, 3, 2, 9, 10, 21, 1, 33, 9, 1};
+        int[] duplicate=new int[arr.length];
+        int startIndex=0;
+        for (int i=0;i<arr.length;i++){
+            for (int j=0;j<arr.length;j++){
+                if (i!=j&&(arr[i]==arr[j])){
+                    if (!isFind(duplicate,arr[i])){
+                        duplicate[startIndex++]=arr[i];
+                    }
+                    break;
+                }
+            }
+        }
+        for (int value:duplicate){
+            if (value!=0){
+                System.out.print(value+", ");
+            }
+        }
+
+    }
+}
