c6bc73b3af36a5fda07d1abe3439bc86690ff1e1
Mehmet Emin AYKAN <77343486+MehmetEmin94@users.noreply.github.com>
2021-08-17 23:50:21 +0300

    Add files via upload

---
+++ src/java102/collections/task1/Book.java
@@ -0,0 +1,61 @@
+package collections.task1;
+
+import java.util.Comparator;
+
+public class Book implements Comparable<Book>, Comparator<Book> {
+    private String bookName;
+    private int pageNum;
+    private String authorName;
+    private String publishedDate;
+
+    public Book(String bookName, int pageNum, String authorName, String publishedDate) {
+        this.bookName = bookName;
+        this.pageNum = pageNum;
+        this.authorName = authorName;
+        this.publishedDate = publishedDate;
+    }
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    @Override
+    public int compareTo(Book o) {
+        String a=this.getBookName();
+        String b=o.getBookName();
+        return a.compareTo(b);
+    }
+
+    @Override
+    public int compare(Book o1, Book o2) {
+        return o1.getPageNum()-o2.getPageNum();
+    }
+}
---
+++ src/java102/collections/task1/Main.java
@@ -0,0 +1,29 @@
+package collections.task1;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Book book_1=new Book("def",50,"kim","2012");
+        Book book_2=new Book("abc",150,"kim kim","1999");
+        Book book_3=new Book("klm",250,"this","1912");
+        Book book_4=new Book("xyz",750,"who","2000");
+        Book book_5=new Book("prs",320,"who who","2015");
+
+        TreeSet<Book> treeSet=new TreeSet<>();
+        treeSet.add(book_1);
+        treeSet.add(book_2);
+        treeSet.add(book_3);
+        treeSet.add(book_4);
+        treeSet.add(book_5);
+
+
+        for (Book b:treeSet) {
+            System.out.println(b.getBookName());
+        }
+
+    }
+}
